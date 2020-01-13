f42390291f4e91c3b1b3f85ac1ff84031b1f6dc4
Jackie <JGtheophila19@gmail.com>
2020-01-13 12:41:25 -0500

    Returns a new string from a specified...
     string in the current instance are replaced with another specified string.

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,15 +6,15 @@ namespace CshapFundamentals
     {
         static void Main(string[] args)
         {
-		var fullName = "Mosh Hamedani";   
-            	//var index = fullName.IndexOf(' ');
-         
-           	var names = fullName.Split(' ');
-            	/*This method splits a string into an array of strings separated by the split delimeters. 
-            	The split delimiters can be a character or an array of characters or an array of strings.*/
-			
-            	Console.WriteLine("FirstName" + name[0]);
-		Console.WriteLine("LastName" + name[1]);
+		var fullName = "Jackie Gut";            
+           	
+		//fullName.Replace("Jackie", "Jackey");
+		//fullName.Replace('o', '0');
+		//fullName.Replace(' ', " ");
+		
+		Console.WriteLine(fullName.Replace("Jackie", "Jackey"););
+		/*Returns a new string in which all occurrences of a specified string 
+		in the current instance are replaced with another specified string.*/
         }
     }
 }
