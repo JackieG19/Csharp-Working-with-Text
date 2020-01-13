6080c414b211d4fca95091d7a62ff67d92407bf3
Jackie <JGtheophila19@gmail.com>
2020-01-13 12:34:43 -0500

    returns an array of strings generated by...
     splitting of original string separated by the delimiters passed as a parameter

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,15 +6,15 @@ namespace CshapFundamentals
     {
         static void Main(string[] args)
         {
-            var fullName = "Mosh Hamedani";   
-            //var index = fullName.IndexOf(' ');
+		var fullName = "Mosh Hamedani";   
+            	//var index = fullName.IndexOf(' ');
          
            	var names = fullName.Split(' ');
-            /*This method splits a string into an array of strings separated by the split delimeters. 
-            The split delimiters can be a character or an array of characters or an array of strings.*/
+            	/*This method splits a string into an array of strings separated by the split delimeters. 
+            	The split delimiters can be a character or an array of characters or an array of strings.*/
 			
-            Console.WriteLine("FirstName" + name[0]);
-			Console.WriteLine("LastName" + name[1]);
+            	Console.WriteLine("FirstName" + name[0]);
+		Console.WriteLine("LastName" + name[1]);
         }
     }
 }
