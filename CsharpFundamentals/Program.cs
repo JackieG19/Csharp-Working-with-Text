5d1696dc9ffcd38a171018990c2e886fc929d41d
Jackie <JGtheophila19@gmail.com>
2020-01-14 11:39:03 -0500

    placing each method under a single object

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -8,23 +8,22 @@ namespace CshapFundamentals
 	{
 		var builder = new StringBuilder("Hello World");
 			
-		builder.Append('-', 10);
-		builder.AppendLine();
-		builder.Append("Header");
-		builder.AppendLine();
-		builder.Append('-', 10);
-		Console.WriteLine(builder);
-			
-		/*Replaces a specified character or string with another specified character or string*/	
-		builder.Replace('-', '+');
+		builder
+			.Append('-', 10);
+			.AppendLine();
 		
-		/*Removes the specified range of characters*/
-		builder.Remove(0, 10);
-			
-		/*Inserts the string of a specified object into a specified character position.*/
-		builder.Insert(0, new string('-'), 10);
+			.Append("Header");
+			.AppendLine();
+		
+			.Append('-', 10);
+		
+			.Replace('-', '+');
+			.Remove(0, 10);
+			.Insert(0, new string('-'), 10);
 			
 		Console.WriteLine(builder);
+			
+		Console.WriteLine("First char: " + builder[0]);
 	}
     }
 }
