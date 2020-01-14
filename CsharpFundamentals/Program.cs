6a80cbbd2bad194a68b45c86a48c329b90b6205e
Jackie <JGtheophila19@gmail.com>
2020-01-14 12:15:13 -0500

    Procedural Programming

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -4,26 +4,49 @@ namespace CshapFundamentals
 {
     class Program
     {
-	static void Main(string[] args)
+	internal class Program
 	{
-		var builder = new StringBuilder("Hello World");
+		public static void Main(string[] args)
+		{	
+			string[] element;
 			
-		builder
-			.Append('-', 10);
-			.AppendLine();
-		
-			.Append("Header");
-			.AppendLine();
-		
-			.Append('-', 10);
-		
-			.Replace('-', '+');
-			.Remove(0, 10);
-			.Insert(0, new string('-'), 10);
+			while(true)
+			{
+				Console.WriteLine("Enter a list of comma-separated numbers: ");
+				var input = Console.ReadLine();
+				
+				if(!String.IsNullOrWhiteSpace(input))
+				{
+					elements = input.Split(',');
+					if(elements.Length >= 5)
+						break;
+				}
+				Console.WriteLine("Invalid List");
+			}
 			
-		Console.WriteLine(builder);
+			var number = new List<int>();
 			
-		Console.WriteLine("First char: " + builder[0]);
+			foreach(var number in elements)
+				number.Add(Convert.TiInt32(number));
+			
+			var smallest = new List<int>();
+			
+			while(smallest.Count < 3)
+			{
+				// Assume the first number is the smallest
+				var min = numbers[0];
+				foreach(var number in numbers)
+				{
+					if(number < min)
+						min = number;
+				}
+				smallest.Add(min);
+				numbers.Remove(min);
+			}
+			Console.WriteLine("The 3 smallest numbers are: ");
+			foreach(var number in smallest)
+				Console.WriteLine(number);
+		}
 	}
     }
 }
